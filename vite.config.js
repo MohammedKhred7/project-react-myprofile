04404a8ff27d412378168b88877af0c2c9c628cf
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-06-09 04:38:05 +0300

    fix: base deploy

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Project-react-myProfile/',
-  assetsDir: 'assets',
-  css: {
-    devSourcemap: true
+  base: "/Project-react-myProfile/",
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.js'],
   },
-  plugins: [
-    vue({
-      template: {
-        transformAssetUrls: {
-          includeAbsolute: false
-        }
-      }
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-})
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   base: "/Project-react-myProfile]/",
-//   plugins: [react()],
-//   test: {
-//     globals: true,
-//     environment: 'jsdom',
-//     setupFiles: ['./src/setupTests.js'],
-//   },
-// });
+});
