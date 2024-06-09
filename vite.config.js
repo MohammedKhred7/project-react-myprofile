384a21a5e741ebd50ab104b08f90b428e0190d12
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-06-09 04:35:23 +0300

    fix: some bug

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,40 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/[Project-react-myProfile]/",
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.js'],
+  base: '/Project-react-myProfile/',
+  assetsDir: 'assets',
+  css: {
+    devSourcemap: true
   },
-});
+  plugins: [
+    vue({
+      template: {
+        transformAssetUrls: {
+          includeAbsolute: false
+        }
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   base: "/Project-react-myProfile]/",
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: ['./src/setupTests.js'],
+//   },
+// });
