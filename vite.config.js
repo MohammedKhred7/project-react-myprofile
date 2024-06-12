f1787fc23c32984798a5cc3819d7911aaf22d4f4
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-06-12 19:57:56 +0300

    fix: delete dir

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base:'project-react-myprofile',
   plugins: [react()],
-  publicDir: 'public'
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
 });
 
