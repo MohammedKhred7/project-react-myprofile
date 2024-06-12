8f0716ded200498c27f7bc525c24a93f8a11aba3
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-06-12 18:18:16 +0300

    fix: edit deploy3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   https:'//github.com/MohammedKhred7.github.io/Project-react-myProfile',
+  base:'/',
   plugins: [react()],
   publicDir: 'public'
 });
