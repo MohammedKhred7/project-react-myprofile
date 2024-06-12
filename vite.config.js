6c5f8c5e7d28097af9b55041087fa2897d23d782
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-06-12 20:33:05 +0300

    fix: deploy

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base:'project-react-myprofile',
   plugins: [react()],
-  build: {
-    outDir: './build'
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
